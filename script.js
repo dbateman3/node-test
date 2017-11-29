b0fe14b9bc4fcd50390f55bb15fe1a549ba583a4
Dan Bateman <dbateman0319@starkstate.net>
2017-11-28 22:34:26 -0500

    Working server

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ const http = require("http");
 const hostName = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader("Content-type", "text/plain");
-	res.end("Hello World");
+const server = http.createServer((request, response) => {
+	const { headers, method, url } = request;
+	let body = [];
+	request.on('error', (err) => {
+		console.error(err);
+	}).on('data', (chunk) => {
+		body.push(chunk);
+	}).on('end', () => {
+		body = Buffer.concat(body).toString();
+		response.on('error', (err) => {
+			console.error(err);
+		});
+		response.statusCode = 200;
+		response.setHeader('Content-Type', 'application/json');
+		const responseBody = { headers, method, url, body };
+		response.write(JSON.stringify(responseBody));
+		response.end();
+	});
 });
 
 server.listen(port, hostName, () => {
